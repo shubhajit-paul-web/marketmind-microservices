b0111591b1b42913ecd3fd614ba2b27584949883
shubhajit-paul-web <shubhajitpaul717@gmail.com>
2025-12-24 22:44:53 +0530

    feat: (Order service) add authentication middleware, order model, and country constants

--- order-service/src/constants/constants.js
+++ order-service/src/constants/constants.js
@@ -1,2 +1,12 @@
 // Database name
 export const DB_NAME = "marketmind-orders";
+export const COUNTRIES = [
+    "india",
+    "united states",
+    "china",
+    "japan",
+    "canada",
+    "russia",
+    "spain",
+    "singapore",
+];
---
+++ order-service/src/models/order.model.js
@@ -0,0 +1,98 @@
+import { Schema, model } from "mongoose";
+import { COUNTRIES } from "../constants/constants.js";
+
+// Address Schema
+const addressSchema = new Schema(
+    {
+        street: {
+            type: String,
+            trim: true,
+            required: true,
+        },
+        city: {
+            type: String,
+            trim: true,
+            required: true,
+        },
+        state: {
+            type: String,
+            trim: true,
+            required: true,
+        },
+        zip: {
+            type: String,
+            trim: true,
+            minLength: 7,
+            maxLength: 7,
+            required: true,
+        },
+        country: {
+            type: String,
+            enum: COUNTRIES,
+            required: true,
+        },
+        landmark: {
+            type: String,
+            trim: true,
+        },
+        typeOfAddress: {
+            type: String,
+            enum: ["work", "home"],
+            required: true,
+        },
+    },
+    { _id: false, timestamps: true }
+);
+
+// Price Schema
+const priceSchema = new Schema(
+    {
+        amount: {
+            type: Number,
+            min: 0,
+            required: true,
+        },
+        currency: {
+            type: String,
+            enum: ["INR", "USD"],
+            default: "INR",
+        },
+    },
+    { _id: false }
+);
+
+const orderSchema = new Schema(
+    {
+        userId: {
+            type: Schema.Types.ObjectId,
+            required: true,
+        },
+        items: [
+            {
+                productId: {
+                    type: Schema.Types.ObjectId,
+                    required: true,
+                },
+                quantity: {
+                    type: Number,
+                    min: 1,
+                    default: 1,
+                },
+                price: priceSchema,
+            },
+        ],
+        totalPrice: priceSchema,
+        status: {
+            type: String,
+            enum: ["PENDING", "CONFIRMED", "SHIPPED", "DELIVERED", "CANCELLED"],
+            default: "PENDING",
+        },
+        shippingAddress: addressSchema,
+    },
+    {
+        timestamps: true,
+    }
+);
+
+const Order = model("Order", orderSchema);
+export default Order;
